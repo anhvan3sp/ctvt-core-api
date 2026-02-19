19bf8118760e0945e42e459d0294ce9dcd9a21d9
anhvan3sp <anhvan3sp@gmail.com>
2026-02-19 20:49:57 +0700

    Create schemas.py

---
+++ app/schemas.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+
+
+class LoginRequest(BaseModel):
+    ma_nv: str
+    password: str
+
+
+class LoginResponse(BaseModel):
+    access_token: str
+    token_type: str
+    ma_nv: str
+    vai_tro: str
