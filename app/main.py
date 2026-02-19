57c5fe7c0a289fc61e26ab8f281bd3a4ac172be9
anhvan3sp <anhvan3sp@gmail.com>
2026-02-19 20:02:16 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,6 @@
 from fastapi import FastAPI
-from app.database import test_connection
 
 app = FastAPI()
-
-@app.on_event("startup")
-def startup():
-    test_connection()
 
 @app.get("/")
 def root():
