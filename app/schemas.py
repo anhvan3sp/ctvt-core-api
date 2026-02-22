bb2cea69b5956f970dad37a422e15c4483557e20
anhvan3sp <anhvan3sp@gmail.com>
2026-02-22 20:28:15 +0700

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -20,3 +20,22 @@ class HoaDonNhapItemCreate(BaseModel):
     ma_sp: str
     so_luong: float = Field(..., gt=0)
     don_gia: float = Field(..., gt=0)
+
+class HoaDonNhapResponse(BaseModel):
+    id: int
+    ngay: date
+    ma_ncc: str
+    ma_nv: str
+    ma_kho: str
+    tong_tien: float
+
+    class Config:
+        from_attributes = True
+
+class HoaDonNhapCreate(BaseModel):
+    ngay: date
+    ma_ncc: str
+    ma_kho: str
+    tien_mat: float = 0
+    tien_ck: float = 0
+    items: List[HoaDonNhapItemCreate]
