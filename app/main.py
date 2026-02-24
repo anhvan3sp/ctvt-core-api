50c1c253e326c96f1de9441334f4d01dd4052d34
anhvan3sp <anhvan3sp@gmail.com>
2026-02-24 19:32:51 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,22 +1,33 @@
-from app.routers import purchase
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from sqlalchemy import text
-from app.routers import auth
+
 from app.database import get_db
+from app.routers import auth, purchase, sale
 from app.auth_utils import get_password_hash
-from app.routers import sale
-app.include_router(sale.router)
+
+
 app = FastAPI()
+
+# ==============================
+# REGISTER ROUTERS
+# ==============================
 app.include_router(auth.router)
 app.include_router(purchase.router)
+app.include_router(sale.router)
 
 
+# ==============================
+# ROOT
+# ==============================
 @app.get("/")
 def root():
     return {"message": "CTVT Core API running"}
 
 
+# ==============================
+# TEST DB
+# ==============================
 @app.get("/test-db")
 def test_db(db: Session = Depends(get_db)):
     try:
@@ -24,6 +35,11 @@ def test_db(db: Session = Depends(get_db)):
         return {"db_status": "connected", "result": result[0]}
     except Exception as e:
         return {"db_status": "error", "detail": str(e)}
+
+
+# ==============================
+# TEST HASH
+# ==============================
 @app.get("/create-hash")
 def create_hash():
     return {"hash": get_password_hash("123abc")}
