974b27cc12470411b27a364bfd0a57401ab45e79
anhvan3sp <anhvan3sp@gmail.com>
2026-02-27 20:00:29 +0700

    Create warehouse.py

---
+++ app/routers/warehouse.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from typing import List
+
+from app.database import get_db
+from app import models, schemas
+
+router = APIRouter(
+    prefix="/warehouses",
+    tags=["Warehouses"]
+)
+
+@router.get("/", response_model=List[schemas.Warehouse])
+def get_warehouses(db: Session = Depends(get_db)):
+    """
+    Lấy toàn bộ kho
+    """
+    warehouses = db.query(models.Warehouse).all()
+    return warehouses
