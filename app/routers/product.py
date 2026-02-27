1b6187bdb45e9e61138789ee204a1b69e1f23651
anhvan3sp <anhvan3sp@gmail.com>
2026-02-27 20:01:16 +0700

    Create product.py

---
+++ app/routers/product.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from typing import List
+
+from app.database import get_db
+from app import models, schemas
+
+router = APIRouter(
+    prefix="/products",
+    tags=["Products"]
+)
+
+@router.get("/", response_model=List[schemas.Product])
+def get_products(db: Session = Depends(get_db)):
+    """
+    Lấy toàn bộ sản phẩm
+    """
+    products = db.query(models.Product).all()
+    return products
