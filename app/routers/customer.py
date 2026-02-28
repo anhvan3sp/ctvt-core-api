8cfff03084ce8fc6ad8b1a1c441ea2e82443c5d3
anhvan3sp <anhvan3sp@gmail.com>
2026-02-28 10:20:33 +0700

    Update customer.py

--- app/routers/customer.py
+++ app/routers/customer.py
@@ -5,7 +5,8 @@ from typing import List
 from app.database import get_db
 from app.models import KhachHang
 from app.schemas import CustomerCreate, CustomerResponse
-
+from sqlalchemy import func
+from app.models import HoaDonBan
 router = APIRouter(prefix="/customer", tags=["Customer"])
 
 
@@ -28,3 +29,17 @@ def create_customer(
 @router.get("/", response_model=List[CustomerResponse])
 def get_customers(db: Session = Depends(get_db)):
     return db.query(KhachHang).all()
+
+@router.get("/debt/{ma_kh}")
+def get_customer_debt(ma_kh: str, db: Session = Depends(get_db)):
+
+    tong_no = db.query(
+        func.coalesce(func.sum(HoaDonBan.no_lai), 0)
+    ).filter(
+        HoaDonBan.ma_kh == ma_kh
+    ).scalar()
+
+    return {
+        "ma_kh": ma_kh,
+        "tong_cong_no": tong_no
+    }
