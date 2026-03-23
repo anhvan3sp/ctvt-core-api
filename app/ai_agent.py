06dc732256cb14a14836e40d5973fe90fc609816
anhvan3sp <anhvan3sp@gmail.com>
2026-03-23 21:29:01 +0700

    Update ai_agent.py

--- app/ai_agent.py
+++ app/ai_agent.py
@@ -1,1 +1,27 @@
+from openai import OpenAI
+import json
+import os
 
+
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY")
+)
+
+
+SYSTEM_PROMPT = """
+Bạn là AI ERP GAS.
+Chỉ trả JSON.
+"""
+
+def ask_ai(user_input):
+    response = client.chat.completions.create(
+        model="gpt-4.1",
+        messages=[
+            {"role": "system", "content": SYSTEM_PROMPT},
+            {"role": "user", "content": user_input}
+        ],
+        response_format={"type": "json_object"},
+        temperature=0
+    )
+
+    return json.loads(response.choices[0].message.content)
